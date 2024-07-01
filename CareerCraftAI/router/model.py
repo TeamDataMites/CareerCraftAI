9f3190763855181eb70e115902355f19ae912449
visith1577 <visithkumarapperuma@gmail.com>
2024-07-01 17:46:47 +0530

    job research

--- CareerCraftAI/router/model.py
+++ CareerCraftAI/router/model.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter, BackgroundTasks
 from schemas.item import PersonalData
 from utils.chat.mindmap.agent import mindmap_agent
 from utils.resume.crew.resume_run import resume_crew
+from utils.chat.report.agent import run_report_agent
 
 router = APIRouter(
     prefix="/prediction",
@@ -50,7 +51,7 @@ async def get_result():
 
 
 @router.get(
-    "/load_direct",
+    "/load_direct/",
     description="fine-tune the cv",
     summary="endpoint to load cv finetune directly"
 )
@@ -64,3 +65,21 @@ async def get_cv(personalData: PersonalData):
     )
 
     return {"result": result}
+
+@router.get('/job_report/',
+            description="get detailed report on the job and company given a description",
+            summary="job report"
+            )
+async def get_job_report(desc: str):
+    report = await run_report_agent(desc)
+    return {"report": report}
+
+@router.post('/lecture_note/', 
+            summary="AI generated lecture note",
+            description="provides a extensivly researched not on a topic of interest."
+            )
+async def generate_lecture(topic: str):
+    pass
+
+
+
