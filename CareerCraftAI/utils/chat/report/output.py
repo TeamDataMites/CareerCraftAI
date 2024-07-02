f3ae9fb7e1e9ab82d24c7af07104448f0c25485e
visith1577 <visithkumarapperuma@gmail.com>
2024-07-02 13:26:15 +0530

    dev branch new

--- CareerCraftAI/utils/chat/report/output.py
+++ CareerCraftAI/utils/chat/report/output.py
@@ -1,7 +1,6 @@
 import os
 import dotenv
-from langchain_core.messages import HumanMessage, ToolMessage
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_core.messages import ToolMessage
 from langchain_core.pydantic_v1 import BaseModel, Field, ValidationError
 
 dotenv.load_dotenv()
@@ -14,7 +13,7 @@ class Reflection(BaseModel):
 class TopicReport(BaseModel):
     """Create Report under the Topic. Provide a Report, reflection, and then follow up with search queries to improve the answer."""
 
-    report: str = Field(description="~200 word detailed report to the job.")
+    report: str = Field(description="~500 word detailed report to the job.")
     reflection: Reflection = Field(description="Your reflection on the initial report.")
     search_queries: list[str] = Field(
         description="1-3 search queries for researching improvements to address the critique of your current answer."
