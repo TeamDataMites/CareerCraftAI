5174e30ccc34dd2c183369a6b88610612238137c
visith1577 <visithkumarapperuma@gmail.com>
2024-07-02 12:48:13 +0530

    dev branch new

--- CareerCraftAI/utils/resume/crew/resume_run.py
+++ CareerCraftAI/utils/resume/crew/resume_run.py
@@ -1,4 +1,5 @@
 from utils.resume.crew.resume_agents import ResumeCrew
+from database.database import db
 
 def resume_crew(linkdin_url, github_url, personal_writeup, job_post, resume):
     inputs = {
@@ -11,3 +12,15 @@ def resume_crew(linkdin_url, github_url, personal_writeup, job_post, resume):
 
     crew = ResumeCrew().crew().kickoff(inputs)
     return crew
+
+
+def resume_save(name, linkdin_url, github_url, personal_writeup, job_post, resume):
+    task_ref = db.collection('tasks').document(name)
+    task_ref.update({'status': 'in progress'})
+
+    result = resume_crew(linkdin_url, github_url, personal_writeup, job_post, resume)
+
+    task_ref.update({
+        'status': 'completed',
+        'result': result
+    })
